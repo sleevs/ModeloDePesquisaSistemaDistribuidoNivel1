898309f69c45bdacbf9804b4c8e59f10329e39de
Jeison Muniz <jsnprogramer@gmail.com>
2024-06-14 16:18:56 -0300

    project strutura

---
+++ sistema-distribuido-no-a/src/main/java/br/com/jsn/sistemadistribuidonoa/SistemaDistribuidoNoAApplication.java
@@ -0,0 +1,13 @@
+package br.com.jsn.sistemadistribuidonoa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDistribuidoNoAApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDistribuidoNoAApplication.class, args);
+	}
+
+}
